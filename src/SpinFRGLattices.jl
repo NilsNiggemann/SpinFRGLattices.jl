4cd56793ba504ac887e514ebc0186f5dbf322040
Nils Niggemann <nils.niggemann@fu-berlin.de>
2021-07-19 14:30:15 +0200

    separated plotting utils

--- src/SpinFRGLattices.jl
+++ src/SpinFRGLattices.jl
@@ -1,22 +1,12 @@
 module SpinFRGLattices
 
 using Parameters,StaticArrays,Test
-version() = "0.3.1"
+version() = "0.3.2"
 
 include("GeometryEssentials.jl")
 
 include("Polymers.jl")
 include("LatticeHelpers.jl")
 include("Lattices/Lattices.jl")
-
-function LatticePlots_activate()
-    @eval Main begin
-        using Parameters,StaticArrays,LaTeXStrings,Plots
-        include(string($(@__DIR__),"/Fourier.jl"))
-        include(string($(@__DIR__),"/LatticePlot.jl"))
-        # export pairsPlot,plotSystem,plotCouplings
-    end
-end
-export LatticePlots_activate
 
 end # module
