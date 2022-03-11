399c77eeff69ff2b4570bf54fef149c509a93c14
Nils Niggemann <niggeni@zedat.fu-berlin.de>
2022-03-11 12:49:59 +0530

    print testing statement before the test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,35 @@
 using SpinFRGLattices,Test
 @test norm([1,1,1]) ≈ sqrt(3)
 
-C2SquareKagome.getC2DimerSquareKagome(6,test = true)
 println("Testing C2SquareKagome")
+C2SquareKagome.getC2DimerSquareKagome(6,test = true)
 
-LargeSquareKagome.getNoMirrDimerSquareKagome(6,test = true)
 println("Testing LargeSquareKagome")
+LargeSquareKagome.getNoMirrDimerSquareKagome(6,test = true)
 
-Pyrochlore.getPyrochlore(6,test=true)
 println("Testing Pyrochlore")
+Pyrochlore.getPyrochlore(6,test=true)
 
-SimpleCubic.getCubic(6,test=true)
 println("Testing SimpleCubic")
+SimpleCubic.getCubic(6,test=true)
 
+println("Testing SquareKagome")
 SquareKagome.getMirrorSquareKagome(6,test=true)
-println("Testing SquareKagome")
 
+println("Testing SquareKagome")
 SquareKagome.getSquareKagome(6,test=true)
-println("Testing SquareKagome")
 
-SquareLattice.getSquareLattice(6,test=true)
 println("Testing SquareLattice")
+SquareLattice.getSquareLattice(6,test=true)
 
-Octochlore.getOctochlore(6,test=true)
 println("Testing Octochlore")
+Octochlore.getOctochlore(6,test=true)
 
+println("Testing Honeycomb")
 Honeycomb.getHoneycomb(6,[1.,2.,3.,4.],test=true)
-println("Testing Honeycomb")
 
+println("Testing Honeycomb")
 Honeycomb.getHoneycombGamma(6,alpha = 1., gamma = 0.3,test=true)
-println("Testing Honeycomb")
 
-TriangularLattice.getTriangularLattice(6,test=true)
 println("Testing TriangularLattice")
+TriangularLattice.getTriangularLattice(6,test=true)
