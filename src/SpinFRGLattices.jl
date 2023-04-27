2bfc4e675e29b70a9da597c0cc84c16654dfaeef
Nils Niggemann <nils.niggemann@fu-berlin.de>
2023-04-27 11:49:47 +0200

    do not export dist anymore since it collides with LinearAlgebra

--- src/SpinFRGLattices.jl
+++ src/SpinFRGLattices.jl
@@ -10,7 +10,7 @@ include("Polymers.jl")
 export getPolymer
 
 include("LatticeHelpers.jl")
-export  Basis_Struct_2D, Basis_Struct_3D,Basis_Struct, Rvec_2D, Rvec_3D, Rvec, getLatticeVec, norm, translateToOrigin, translation, getCartesian, aboveLine, belowLine,aboveLine_strict, belowLine_strict, MirrorLine, dist, generatePairSites,generateLUnitCells,Mirror,printPairs
+export  Basis_Struct_2D, Basis_Struct_3D,Basis_Struct, Rvec_2D, Rvec_3D, Rvec, getLatticeVec, translateToOrigin, translation, getCartesian, aboveLine, belowLine,aboveLine_strict, belowLine_strict, MirrorLine, dist, generatePairSites,generateLUnitCells,Mirror,printPairs
 
 include("ReduceGeometry.jl")
 export MapToPair, setCoupling!, setNeighborCouplings!, getSiteType, testGeometry, findSymmetryReduced, pairToInequiv_vec,generatePairToInequiv, getLatticeGeometry, getFRGComplexity
